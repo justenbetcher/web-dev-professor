2c813718eb397fd5eb28b4d5eb4feeedf30cff47
Moshe Plotkin <mplotkin@jewpaltz.com>
2023-03-21 14:54:46 -0400

    working api

--- server/index.js
+++ server/index.js
@@ -5,7 +5,11 @@ const app = express()
 const hostname = '127.0.0.1';
 const port = process.env.PORT || 3000;
 
+// Middleware
+app.
+    use(express.json());
 
+// Actions
 app
     .get('/', (req, res) => {
         res.send('Hello World! From Express')
